75e72128a7c09518fcc0467cac27146a601e0dd0
NoSkillGuy <rsivapraveen001@gmail.com>
2018-08-07 17:16:29 +0530

    Added Faker gem to development and test environment Removed tzinfo-data gem from Gemfile Changed the database timezone to 'New Delhi' Changed the test environment to mysql Added the seed file to load basic data into database

--- Gemfile
+++ Gemfile
@@ -42,6 +42,9 @@ gem 'mysql2'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+
+  # Added faker gem to generate random names 
+  gem 'faker', '~> 1.6', '>= 1.6.3', require: false
 end
 
 group :development do
@@ -62,4 +65,4 @@ group :test do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+# gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+User.create(first_name: 'Siva Praveen', second_name: 'Rayapudi', email: 'rsivapraveen001@gmail.com', role: 0, phone_number: '8008270250')
+20.times do |i|
+	fn,sn = Faker::FunnyName.two_word_name.split(' ')
+	User.create(first_name: fn, second_name: sn, email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number)
+end
+
+data_templates = [
+	'high risk',
+	'mother',
+	'child',
+	'refer',
+	'treatment',
+	'delivery',
+	'normal',
+	'teek',
+	'alert',
+	'HB',
+	'iron',
+	"MAUN #{Faker::Number.between(1, 20)}"
+]
+
+20.times do |t|
+	tm = Time.now - t*60
+	10.times do |i|
+		WhatsppMessage.create(data: "#{Faker::HarryPotter.quote} #{data_templates.sample}", created_at: tm, updated_at: tm, user_id: Faker::Number.between(2, 21))
+	end
+end
